8c2ca95508fc1bce2d9e3a9d881a06f223402ea3
vuthao <vuthao27@gmail.com>
2013-12-19 19:46:08 +0700

    Thêm ví dụ xtemplate

---
+++ xtpl/list.php
@@ -0,0 +1,37 @@
+<?php
+
+// Xac dinh thu muc goc cua site
+define( 'NV_ROOTDIR', pathinfo( str_replace( DIRECTORY_SEPARATOR, '/', __file__ ), PATHINFO_DIRNAME ) );
+
+include NV_ROOTDIR . '/xtemplate.class.php';
+
+$array = array( );
+$array[] = array(
+	'id' => '1',
+	'name' => 'Nguyễn Văn A',
+	'date' => '20/11/2010'
+);
+
+$array[] = array(
+	'id' => '2',
+	'name' => 'Nguyễn Văn b',
+	'date' => '20/11/2010'
+);
+
+$array[] = array(
+	'id' => '3',
+	'name' => 'Nguyễn Văn c',
+	'date' => '20/11/2010'
+);
+
+$xtpl = new XTemplate( 'list.html' );
+foreach( $array as $value )
+{
+	$xtpl->assign( 'DATA', $value );
+	$xtpl->parse( 'main.loop' );
+}
+$xtpl->parse( 'main' );
+$cont = $xtpl->text('main');
+
+echo 'Thực hiên' .$cont;
+?>
